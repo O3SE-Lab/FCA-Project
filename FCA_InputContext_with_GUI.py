5637d3796ef31e37d851b3101c565c72ebfbbdc3
TaeYeong KIM <52776473+TaeYeong14@users.noreply.github.com>
2021-05-24 13:39:01 +0900

    Update FCA_InputContext_with_GUI.py

--- FCA_InputContext_with_GUI.py
+++ FCA_InputContext_with_GUI.py
@@ -1,68 +1,80 @@
+# FCA InputContext with gui
+# 2018210032 김태영
+# 2021.05.23
+
 import pandas as pd
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QDesktopWidget, QGridLayout, QPushButton, QFileDialog, QLabel, QTextBrowser
-
 
 class FCAInputContextGUI(QWidget):
 
     def __init__(self):
         super().__init__()
-        self.num = 0
+
+        # 기본 창 설정
         self.setWindowTitle('FCA InputContext GUI')
         self.resize(800, 600)
         self.center()
 
-        self.data = 0
+        # 그리드 레이아웃을 사용한 위젯 배치 및 설정
+        self.grid = QGridLayout()
+        self.setLayout(self.grid)
 
-        self.Lgrid = QGridLayout()
-        self.setLayout(self.Lgrid)
+        # 라벨 생성 및 설정
+        self.titleL = QLabel('FCA Input Context', self)  # 타이틀 라벨
+        self.filePathL = QLabel('', self)  # 선택한 파일 경로를 보여줄 라벨
+        self.resultTitleL = QLabel('[실행결과]', self)  # 실행결과 타이틀 라벨
 
-        self.label1 = QLabel('FCA Input Context', self)
-        self.label2 = QLabel('', self)
-        self.label3 = QLabel('[실행결과]', self)
+        self.grid.addWidget(self.titleL, 0, 0)
+        self.grid.addWidget(self.filePathL, 2, 0)
+        self.grid.addWidget(self.resultTitleL, 3, 0)
 
-        self.Lgrid.addWidget(self.label1, 0, 0)
+        # 파일 오픈 버튼 생성 및 설정
+        self.fileOpenB = QPushButton('Context 입력(Open CSV File)', self)
+        self.grid.addWidget(self.fileOpenB, 1, 0)
+        self.fileOpenB.clicked.connect(self.csv_open)
 
-        self.addbutton1 = QPushButton('Context 입력(Open CSV File)', self)
-        self.Lgrid.addWidget(self.addbutton1, 1, 0)
-        self.addbutton1.clicked.connect(self.csv_open)
+        # 실행 버튼 생성 및 설정
+        self.InputContextB = QPushButton('실행', self)
+        self.grid.addWidget(self.InputContextB, 1, 1)
+        self.InputContextB.clicked.connect(lambda: self.InputContext(self.filePathL.text()))
 
-        self.addbutton2 = QPushButton('실행', self)
-        self.Lgrid.addWidget(self.addbutton2, 1, 1)
-        self.addbutton2.clicked.connect(lambda: self.InputContext(self.label2.text()))
-
-        self.Lgrid.addWidget(self.label2, 2, 0)
-        self.Lgrid.addWidget(self.label3, 3, 0)
-
-        self.resultsh = QTextBrowser()
-        self.Lgrid.addWidget(self.resultsh, 4, 0)
+        # 실행 결과 출력 화면 생성 및 설정
+        self.resultS = QTextBrowser()
+        self.grid.addWidget(self.resultS, 4, 0)
 
         self.show()
 
-    def csv_open(self):
-        FileOpen = QFileDialog.getOpenFileName(self, 'Open file', './', 'csv(*.csv)')
-        self.label2.setText(FileOpen[0])
-
-    def InputContext(self, FilePath):
-        self.data = pd.read_csv(FilePath)
-        G = set(self.data.index)
-        M = set(self.data.columns)
-        I = set()
-
-        # Process : Formal Context를 읽어들여서 "적절한 Data구조 K"에 저장". formal context K:=(G, M, I)
-        for g in G:
-            for m in M:
-                if self.data[m][g] in ['x', 'X']:
-                    I.add((g, m))
-
-        K = [G, M, I]
-        self.resultsh.append(str(K))
-
+    # 창을 화면 가운데에 띄우는 함수
     def center(self):
         qr = self.frameGeometry()
         cp = QDesktopWidget().availableGeometry().center()
         qr.moveCenter(cp)
         self.move(qr.topLeft())
+
+    # csv file open 및 filePath 출력 함수
+    def csv_open(self):
+        fname = QFileDialog.getOpenFileName(self, 'Open file', './', 'csv(*.csv)')
+        self.filePathL.setText(fname[0])
+
+    # Input Context 실행 함수
+    def InputContext(self, fPath):
+
+        data = pd.read_csv(fPath)
+
+        # G : 객체들의 집합, M : 속성들의 집합, I : 객체와 속성 사이의 관계 집합 (I ⊆ G × M)
+        G = set(data.index)
+        M = set(data.columns)
+        I = set()
+
+        # Process : Formal Context를 읽어들여서 "적절한 Data구조 K"에 저장". formal context K:=(G, M, I)
+        for g in G:
+            for m in M:
+                if data[m][g] in ['x', 'X']:
+                    I.add((g, m))
+
+        K = [G, M, I]
+        self.resultS.append(str(K))
 
 
 if __name__ == '__main__':
