cfb498810b2aeea4a4deb7e7337944908bbff673
TaeYeong KIM <52776473+TaeYeong14@users.noreply.github.com>
2021-07-06 15:18:27 +0900

    Upload FCA_with_GUI_prototype.py

--- FCA_with_GUI_prototype.py
+++ FCA_with_GUI_prototype.py
@@ -8,6 +8,8 @@
 # [이번주 진행할 사항]
 # FCA 프로그램 제작 완료하기(일부 변수명 의미있게 수정, CA CO 구현 부분 문제 해결 및 구현 완료하기, Context 입력(open csv file) 부분을 탭 밖(탭 윗부분)에 배치하여 Context 입력은 한번만 해도 되도록 배치, Formal Concept GUI & 구현 가능하게)
 
+# 만들 때 참고한 사이트 : https://wikidocs.net/book/2165
+
 import pandas as pd
 import sys
 from PyQt5.QtWidgets import *
@@ -16,6 +18,7 @@ from PyQt5.QtWidgets import *
 # Dialog : MainWindow의 특별한 종류. 대화상자가 항상 별도의 창에 표시되는 최상위 위젯(다른 위젯에 넣을 수 없음)
 # MainWindow : 메뉴바, 스테이터스바 등 미리 설정되어있는 최상위 위젯
 
+#-----------------------------------------------
 # Main
 class FCAwithGUI(QWidget):
     def __init__(self):
@@ -46,7 +49,10 @@ class FCAwithGUI(QWidget):
         cp = QDesktopWidget().availableGeometry().center()
         qr.moveCenter(cp)
         self.move(qr.topLeft())
+#-----------------------------------------------
 
+
+#-----------------------------------------------
 # InputContext Tab
 class ICTab(QWidget):
     def __init__(self):
@@ -54,37 +60,42 @@ class ICTab(QWidget):
         self.initUI()
 
     def initUI(self):
-        # InputContext - 라벨 생성
-        ICtitleL = QLabel('[ Context 입력 ]')
+
+        # InputContext - 라벨들 생성
+        ICtitleL = QLabel('[ Context 입력 ]') # context 입력 타이틀 라벨
         ICfptitleL = QLabel('  선택 파일(*.csv) : ', self)  # 선택 파일 타이틀 라벨
         self.ICfilePathL = QLabel('', self)  # 선택한 파일 경로를 보여줄 라벨
+
         ICresultTitleL = QLabel('[ Input Context 실행결과 ]', self)  # IC 실행결과 타이틀 라벨
 
         # InputContext - 실행 결과 출력할 텍스트 브라우저 생성
         self.ICtb = QTextBrowser()
 
         # InputContext - 파일 오픈 및 실행, CLEAR 버튼 생성
-        ICfileOpenB = QPushButton('Context 입력(Open CSV File)', self)
-        ICfileOpenB.clicked.connect(self.csv_open)
-        ICexcuteB = QPushButton('실행', self)
-        ICexcuteB.clicked.connect(self.IC_clear_text)
-        ICexcuteB.clicked.connect(lambda: self.ICresult(self.InputContext(self.ICfilePathL.text())))
-        clearB = QPushButton('CLEAR', self)
-        clearB.clicked.connect(self.IC_clear_text)
+        ICfileOpenB = QPushButton('Context 입력(Open CSV File)', self) # context 입력 버튼
+        ICfileOpenB.clicked.connect(self.csv_open) # 버튼 클릭 시 csv_open() 실행
+
+        ICexcuteB = QPushButton('실행', self) # 실행 버튼
+        ICexcuteB.clicked.connect(self.IC_clear_text) # 실행버튼 클릭 시 결과 출력되는 텍스트 브라우저 초기화(IC_clear_text())
+        ICexcuteB.clicked.connect(lambda: self.ICresult(self.InputContext(self.ICfilePathL.text()))) # 실행버튼 클릭 시 ICresult(InputContext(ICfilePathL.text())) 실행
+
+        clearB = QPushButton('CLEAR', self) # clear 버튼
+        clearB.clicked.connect(self.IC_clear_text) # clear 버튼 클릭 시 텍스트 브라우저 초기화(IC_clear_text())
 
         # 박스레이아웃을 사용한 배치
-        # 선택 파일 경로 표시
-        Lhbox = QHBoxLayout()
-        Lhbox.addWidget(ICfptitleL)
-        Lhbox.addWidget(self.ICfilePathL)
-        Lhbox.addStretch()
+        # InputContext - 선택 파일 경로 표시
+        Lhbox = QHBoxLayout() # 선택 파일 : 파일경로(.csv) 나올 수 있게 수평 박스(hbox)에 배치. 라벨이라 L붙여서 변수명을 Lhbox로 정함
+        Lhbox.addWidget(ICfptitleL) # '선택파일' 라벨
+        Lhbox.addWidget(self.ICfilePathL) # 파일경로 라벨
+        Lhbox.addStretch() # 나머지 빈공간
 
-        # Context open 실행 및 Clear 버튼
-        Bhbox = QHBoxLayout()
-        Bhbox.addWidget(ICfileOpenB)
-        Bhbox.addWidget(ICexcuteB)
-        Bhbox.addWidget(clearB)
+        # InputContext - Context open 실행 및 Clear 버튼
+        Bhbox = QHBoxLayout() # context open 버튼, 실행 버튼, clear 버튼 수평 박스(hbox)에 배치. 버튼이라 B붙여서 변수명을 Bhbox로 정함
+        Bhbox.addWidget(ICfileOpenB) # context open 버튼
+        Bhbox.addWidget(ICexcuteB) # 실행 버튼
+        Bhbox.addWidget(clearB) # clear 버튼
 
+        # 수직 박스(vbox)에 다 넣어줌
         vbox = QVBoxLayout()
         vbox.addWidget(ICtitleL)
         vbox.addLayout(Lhbox)
@@ -95,9 +106,10 @@ class ICTab(QWidget):
 
     # Input Context 실행 함수
     def InputContext(self, fPath):
-        data = pd.read_csv(fPath)
 
-        # G : 객체들의 집합, M : 속성들의 집합, I : 객체와 속성 사이의 관계 집합 (I ⊆ G × M)
+        data = pd.read_csv(fPath) # fPath = 선택한 파일 경로
+
+        # G : 객체들의 집합, M : 속성들의 집합, I : 객체와 속성 사이의 관계 집합 (I ⊆ G × M). 따라서 list가 아닌 set으로.
         G = set(data.index)
         M = set(data.columns)
         I = set()
@@ -107,7 +119,9 @@ class ICTab(QWidget):
             for m in M:
                 if data[m][g] in ['x', 'X']:
                     I.add((g, m))
+
         K = [G, M, I]
+
         return K
 
     def ICresult(self, K):
@@ -115,24 +129,30 @@ class ICTab(QWidget):
         M = K[1]
         I = K[2]
 
+        # G 결과 출력
         self.ICtb.append(" G ".center(110, '-'))
         self.ICtb.append(str(G))
+        # M 결과 출력
         self.ICtb.append(" M ".center(110, '-'))
         self.ICtb.append(str(M))
-        self.ICtb.append(" I ".center(110, '-'))
+        # I 결과 출력
+        self.ICtb.append(" I ".center(111, '-'))
         self.ICtb.append(str(I))
-        self.ICtb.append(" K ".center(110, '-'))
+        # K 결과 출력
+        self.ICtb.append(" K ".center(111, '-'))
         self.ICtb.append(str(K))
 
     def csv_open(self):
-        fname = QFileDialog.getOpenFileName(self, 'Open file', './', 'csv(*.csv)')
-        self.ICfilePathL.setText(fname[0])
-        self.ICtb.clear()
+        fname = QFileDialog.getOpenFileName(self, 'Open file', './', 'csv(*.csv)') # 세번쨰 매개변수는 기본경로 설정. 안하면  모든파일(*)로 됨
+        self.ICfilePathL.setText(fname[0]) # 파일 경로 보여주는 라벨의 text를 파일 경로로 설정
+        self.ICtb.clear() # 결과창(텍스트 브라우저) clear
 
     # ICtextBrowser clear 함수
     def IC_clear_text(self):
         self.ICtb.clear()
+#-----------------------------------------------
 
+#-----------------------------------------------
 # CA CO Tab
 class CACOTab(QWidget):
     def __init__(self):
@@ -141,38 +161,50 @@ class CACOTab(QWidget):
 
     def initUI(self):
         # InputContext - 라벨, 버튼, 텍스트브라우저 생성
-        ICtitleL = QLabel('[ Context 입력 ]')
+        ICtitleL = QLabel('[ Context 입력 ]') # context 입력 타이틀 라벨
         ICfptitleL = QLabel('  선택 파일(*.csv) : ', self)  # 선택 파일 타이틀 라벨
         self.ICfilePathL = QLabel('', self)  # 선택한 파일 경로를 보여줄 라벨
+
         ICresultTitleL = QLabel('[ Input Context 실행결과 ]', self)  # IC 실행결과 타이틀 라벨
         self.ICtb = QTextBrowser() # IC 실행 결과 출력할 텍스트 브라우저
-        ICfileOpenB = QPushButton('Context 입력(Open CSV File)', self)
-        ICfileOpenB.clicked.connect(self.csv_open)
-        ICexcuteB = QPushButton('실행', self)
-        ICexcuteB.clicked.connect(self.IC_clear_text)
-        ICexcuteB.clicked.connect(lambda: self.ICresult(self.InputContext(self.ICfilePathL.text())))
-        clearB = QPushButton('CLEAR', self)
-        clearB.clicked.connect(self.IC_clear_text)
+
+        ICfileOpenB = QPushButton('Context 입력(Open CSV File)', self) # context 입력 버튼
+        ICfileOpenB.clicked.connect(self.csv_open) # 버튼 클릭 시 csv_open() 실행
+
+        ICexcuteB = QPushButton('실행', self) # 실행 버튼
+        ICexcuteB.clicked.connect(self.IC_clear_text) # 버튼 클릭 시 결과창 clear 후
+        ICexcuteB.clicked.connect(lambda: self.ICresult(self.InputContext(self.ICfilePathL.text()))) # 결과 출력
+
+        clearB = QPushButton('CLEAR', self) # clear 버튼
+        clearB.clicked.connect(self.IC_clear_text) # 버튼 클릭 시 ICtb(결과창(텍스트브라우저)) clear
+
+        # 구분 경계선 --- 이런거
         jtextL = QLabel('-' * 118)
 
         #CA
         jtext2L = QLabel('-' * 118)
+
         # CACO - 라벨 생성 및 설정
         CAtitleL = QLabel('[CA]', self)  # CA 타이틀 라벨
         CAresultTitleL = QLabel('[CA 실행결과]', self)  # CA 실행결과 타이틀 라벨
+
         COtitleL = QLabel('[CO]', self)  # CO 타이틀 라벨
         COresultTitleL = QLabel('[CO 실행결과]', self)  # CO 실행결과 타이틀 라벨
 
-        self.CAte = QTextEdit()
-        self.CAte.setAcceptRichText(False)
-        self.COte = QTextEdit()
+        # 텍스트 에디터 생성 및 설정
+        self.CAte = QTextEdit() # CA 입력받을 텍스트에디터
+        self.CAte.setAcceptRichText(False) # setAcceptRichText를 False로 하면 모두 플레인 텍스트로 인식
+        self.COte = QTextEdit() # CO 입력받을 텍스트에디터
+        self.COte.setAcceptRichText(False)  # setAcceptRichText를 False로 하면 모두 플레인 텍스트로 인식
 
+        # 버튼 생성 및 설정
+        # CA
         CAexcuteB = QPushButton('실행', self)
         CAclearB = QPushButton('CLEAR', self)
-
         CAexcuteB.clicked.connect(self.CA_clear_text)
         CAexcuteB.clicked.connect(lambda: self.CA(self.CAte.toPlainText()))
 
+        # CO
         COexcuteB = QPushButton('실행', self)
         COclearB = QPushButton('CLEAR', self)
         COexcuteB.clicked.connect(self.CO_clear_text)
@@ -184,7 +216,7 @@ class CACOTab(QWidget):
         # InputContext - 실행 결과 출력 화면 생성
         self.COtb = QTextBrowser()
 
-        # 박스레이아웃을 사용한 배치
+        # 박스레이아웃을 사용한 배치 (변수명 좀 더 의미있게 수정 필요)
         hbox1 = QHBoxLayout()
         hbox1.addWidget(ICfptitleL)
         hbox1.addWidget(self.ICfilePathL)
@@ -238,6 +270,7 @@ class CACOTab(QWidget):
         self.setLayout(vbox)
 
     def InputContext(self, fPath):
+
         data = pd.read_csv(fPath)
 
         # G : 객체들의 집합, M : 속성들의 집합, I : 객체와 속성 사이의 관계 집합 (I ⊆ G × M)
@@ -250,7 +283,9 @@ class CACOTab(QWidget):
             for m in M:
                 if data[m][g] in ['x', 'X']:
                     I.add((g, m))
+
         K = [G, M, I]
+
         return K
 
     def ICresult(self, K):
@@ -262,22 +297,23 @@ class CACOTab(QWidget):
         self.ICtb.append(str(G))
         self.ICtb.append(" M ".center(110, '-'))
         self.ICtb.append(str(M))
-        self.ICtb.append(" I ".center(110, '-'))
+        self.ICtb.append(" I ".center(111, '-'))
         self.ICtb.append(str(I))
-        self.ICtb.append(" K ".center(110, '-'))
+        self.ICtb.append(" K ".center(111, '-'))
         self.ICtb.append(str(K))
 
-    def CA(self, inA):
+    # CA 검토 or 수정 필요
+    def CA(self, A):
         K = self.InputContext(self.ICfilePathL.text())
         I = K[2]
 
         M = []
         A_Prime = []
 
-        A = []
-        A = inA.split(',')
+        AL = []
+        AL = A.split(',')
 
-        for a in A:
+        for a in AL:
             # 임시적으로 저장할 리스트
             temp = []
 
@@ -305,48 +341,46 @@ class CACOTab(QWidget):
                 # is_in이 True일경우 A_Prime에 넣음
                 if is_in:
                     A_Prime.append(m)
+
         self.CAtb.append(A_Prime)
+
         return A_Prime
 
-    def CO(self, inB):
-        K = self.InputContext(self.ICfilePathL.text())
-        I = K[2]
-
+    # CO 검토 or 수정 필요
+    def CO(self, B):
+        # g들을 집어넣을 리스트
         G = []
-        B_Prime = []
 
-        B = []
-        B = inB.split(',')
+        # B_Prime 리스트
+        B_Prime = []
 
         for b in B:
             # 임시적으로 저장할 리스트
             temp = []
 
-            # I에서 A의 요소들의 속성들을 가져와 M에 저장
-            for i in I:
-                if i[0] == b:
+            # I에서 B의 요소들의 속성들을 가져와 G에 저장
+            for i in K[2]:
+                if i[1] == b:
                     temp.append(i[0])
             G.append(temp)
 
-        if len(G) != 0:  # 공집합일 경우를 제외
+        # G에서 비교하여 공통적인 g들을 구함
+        for g in G[0]:
+            is_in = False  # 모두 포함하는지 확인할 boolean
+            for i in range(len(G)):
+                if g in G[i]:
+                    if i == (len(G) - 1):  # 마지막까지 포함하면 is_in을 True로 바꿈
+                        is_in = True
+                    continue
 
-            # M에서 비교하여 공통적인 m들을 구함
-            for m in G[0]:
-                is_in = False  # 모두 포함하는지 확인할 boolean
-                for i in range(len(G)):
-                    if m in G[i]:
-                        if i == (len(G) - 1):  # 마지막까지 포함하면 is_in을 True로 바꿈
-                            is_in = True
-                        continue
+                # 없는경우 에는 루프를 끊음
+                else:
+                    break
 
-                    # 없는경우 에는 루프를 끊음
-                    else:
-                        break
+            # is_in이 True일경우 B_Prime에 넣음
+            if is_in:
+                B_Prime.append(g)
 
-                # is_in이 True일경우 A_Prime에 넣음
-                if is_in:
-                    B_Prime.append(m)
-        self.COtb.append(B_Prime)
         return B_Prime
 
 
@@ -366,7 +400,10 @@ class CACOTab(QWidget):
     # COtextBrowser clear 함수
     def CO_clear_text(self):
         self.COtb.clear()
+#-----------------------------------------------
 
+#-----------------------------------------------
+# FormalConcept tab
 class FCTab(QWidget):
     def __init__(self):
         super().__init__()
@@ -374,8 +411,9 @@ class FCTab(QWidget):
 
     def initUI(self):
         pass
+#-----------------------------------------------
 
-
+# main 실행
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = FCAwithGUI()
